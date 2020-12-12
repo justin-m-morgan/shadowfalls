1076f90dd561174d619021c95435c4af0211e602
Justin Morgan <justin.morgan@hey.com>
2020-12-11 21:45:08 -0800

    :wrench: (deploy) Config for Heroku deploy
    Following the guide directions from the phoenix framework website
    
    Closes #1

--- lib/shadowfalls_web/endpoint.ex
+++ lib/shadowfalls_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule ShadowfallsWeb.Endpoint do
   ]
 
   socket "/socket", ShadowfallsWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]

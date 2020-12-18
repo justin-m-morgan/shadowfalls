8dff2dbd110a46b7579bf39abf043def8be447f3
Justin Morgan <justin.morgan@hey.com>
2020-12-18 13:59:16 -0800

    :wrench: (deploy) Modify prod.exs to allow for env config deploy url

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :shadowfalls, ShadowfallsWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "infinite-basin-65030.herokuapp.com", port: 443],
+  url: [scheme: "https", host: System.get_env("DEPLOY_URL"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 

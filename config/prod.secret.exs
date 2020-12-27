891157d0933dfbd0e3baa650139102b732dea506
Justin Morgan <justin.morgan@hey.com>
2020-12-27 11:41:24 -0800

    :wrench: (Gigalixir) Update prod configuration per gigalixir docs

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -12,9 +12,10 @@ database_url =
     """
 
 config :shadowfalls, Shadowfalls.Repo,
+  adapter: Ecto.Adapters.Postgres,
   ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
